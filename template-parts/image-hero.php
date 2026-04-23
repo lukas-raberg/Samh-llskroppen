7be14e0589ebb764f152e7c9708109ce3d5b8962
Lukas <lukas.raberg@gmail.com>
2026-04-23 13:24:32 +0200

    Template part image hero

---
+++ template-parts/image-hero.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Template part för att visa bilder med suddig bakgrund (Aspect Ratio 16:9)
+ * Används i index.php, page-all.php och single.php
+ */
+if ( has_post_thumbnail() ) : 
+    $thumb_id = get_post_thumbnail_id();
+    $meta = wp_get_attachment_metadata($thumb_id);
+    $image_url = get_the_post_thumbnail_url(get_the_ID(), 'large');
+    
+    // Kolla om bilden är stående
+    $is_portrait = false;
+    if ( isset($meta['width'], $meta['height']) && $meta['height'] > $meta['width'] ) {
+        $is_portrait = true;
+    }
+    
+    $container_class = $is_portrait ? 'glass-container is-portrait' : 'glass-container is-landscape';
+    ?>
+
+    <div class="<?php echo $container_class; ?>">
+        <div class="glass-background" style="background-image: url('<?php echo $image_url; ?>');"></div>
+        <?php the_post_thumbnail('large', ['class' => 'glass-foreground']); ?>
+    </div>
+
+<?php endif; ?>
