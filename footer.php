f59088b86178608e4fa3df1cb858339bd20627f2
Lukas <lukas.raberg@gmail.com>
2026-04-23 15:09:34 +0200

    Lagt till manifest-text i footern, justerat vikt på footer p

--- footer.php
+++ footer.php
@@ -3,8 +3,8 @@
                 <p class="title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php echo esc_html( get_bloginfo( 'name' ) ); ?></a></p>
                 <p class="subtitle"><?php bloginfo( 'description' ); ?></p>
                 <hr>
-                <h2>Manifest?</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <h2>Manifest</h2>
+                <p>Samhällskroppen är en progressiv tidskrift för vårdpolitisk debatt som tar sjukvårdspolitikens ödesfrågor till ett tillräckligt djup för att argument ska vässas på riktigt. Tilltalet ska vara tillgängligt för att nå den breda krets av patienter och vårdpersonal som måste ingå en politisk allians om den behovsstyrda och solidariskt finansierade vården ska finnas kvar. Samhällskroppen görs av vårdpersonal och forskare för patienter, vårdarbetare, och allmänhet med engagemang i vårdfrågor.</p>
                 <hr>
                 <?php
                     wp_nav_menu( array(
