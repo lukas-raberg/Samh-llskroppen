6511574593fb3e86cd1509ce188bd9c9057623d3
Lukas <lukas.raberg@gmail.com>
2026-04-23 13:44:27 +0200

    DRY för rubriksektioner

--- index.php
+++ index.php
@@ -1,25 +1,18 @@
 <?php
 get_header(); ?>
 <main id="primary-content">
+
 <?php if ( have_posts() ) :
     while ( have_posts() ) : the_post(); ?>
-        
-        <?php ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class( 'start' ); ?>>
 
-        <?php kroppsam_post_categories(); ?>
-        
+         <?php kroppsam_post_categories( 'theme' ); ?>
+
         <?php get_template_part( 'template-parts/image-hero' ); ?>
             
-        <a href="<?php echo esc_url( get_permalink() ); ?>" class="article-header-link">
-            <?php 
-            echo get_the_tag_list('<span class="eyebrow">', ', ', '</span> '); 
-            ?>
-            <h2 class="entry-title"><?php the_title(); ?></h2>
-       </a>
+        <?php get_template_part( 'template-parts/entry-header' ); ?>
 
         <?php the_excerpt(); ?> 
-
 
         </article>
 
--- single.php
+++ single.php
@@ -5,28 +5,15 @@ get_header(); ?>
 <?php if ( have_posts() ) :
     while ( have_posts() ) : the_post(); ?>
         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            
-            <?php kroppsam_post_categories(); ?>
+            <?php kroppsam_post_categories( 'theme' ); ?>
 
             <?php get_template_part( 'template-parts/image-hero' ); ?>
 
-            <div class="entry-header-single">
-                <?php 
-                echo get_the_tag_list('<span class="eyebrow">', ', ', '</span> '); 
-                ?>
-                <h1 class="entry-title"><?php the_title(); ?></h1>
-            </div>
+            <?php get_template_part( 'template-parts/entry-header' ); ?>
 
             <div class="entry-content">
                 <?php the_content(); ?>
             </div>
-
-            <?php
-            wp_link_pages( array(
-                'before' => '<nav aria-label="' . esc_attr__( 'Sidor', 'samkropp' ) . '">',
-                'after'  => '</nav>',
-            ) );
-            ?>
         </article>
         
         <nav class="navigation post-navigation">
