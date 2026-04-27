d2ca8aa6b3bd695f1cfafc2dcee0abd2a8274f2f
Lukas <lukas.raberg@gmail.com>
2026-04-27 10:44:25 +0200

    Uppdaterat image-hero och css för bättre bildtextstöd

--- template-parts/image-hero.php
+++ template-parts/image-hero.php
@@ -8,6 +8,9 @@ if ( has_post_thumbnail() ) :
     $meta = wp_get_attachment_metadata($thumb_id);
     $image_url = get_the_post_thumbnail_url(get_the_ID(), 'large');
     
+    // Hämta bildtexten (Caption) från mediabiblioteket
+    $caption = get_post_field('post_excerpt', $thumb_id);
+    
     // Kolla om bilden är stående
     $is_portrait = false;
     if ( isset($meta['width'], $meta['height']) && $meta['height'] > $meta['width'] ) {
@@ -17,9 +20,23 @@ if ( has_post_thumbnail() ) :
     $container_class = $is_portrait ? 'glass-container is-portrait' : 'glass-container is-landscape';
     ?>
 
-    <div class="<?php echo $container_class; ?>">
-        <div class="glass-background" style="background-image: url('<?php echo $image_url; ?>');"></div>
-        <?php the_post_thumbnail('large', ['class' => 'glass-foreground']); ?>
-    </div>
+    <?php 
+        // Skapa en extra klass om det finns text
+        $caption_class = !empty($caption) ? 'has-text' : 'is-empty'; 
+        ?>
+
+        <figure class="hero-figure">
+            <div class="<?php echo esc_attr($container_class); ?>">
+                <div class="glass-background" style="background-image: url('<?php echo esc_url($image_url); ?>');"></div>
+                <?php the_post_thumbnail('large', ['class' => 'glass-foreground']); ?>
+            </div>
+
+            <figcaption class="hero-caption <?php echo $caption_class; ?>">
+                <?php echo wp_kses_post($caption); ?>
+            </figcaption>
+        </figure>
 
 <?php endif; ?>
+
+
+
